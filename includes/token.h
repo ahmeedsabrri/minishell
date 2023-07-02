cb750b9ab4a7217fe97ce47c2a9e31372f5fe469
asabri <asabri@asabri.myguest.virtualbox.org>
2023-07-02 02:23:03 +0100

    not done yet

--- includes/token.h
+++ includes/token.h
@@ -6,7 +6,7 @@
 /*   By: asabri <asabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 02:36:54 by asabri            #+#    #+#             */
-/*   Updated: 2023/06/14 02:55:11 by asabri           ###   ########.fr       */
+/*   Updated: 2023/06/29 04:14:29 by asabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 typedef struct s_token
 {
     char *token;
+    char *type;
+    bool hd;
+    bool  expand;
     struct s_token *next;
     struct s_token *bottom;
 }   t_token;
--- src/token/tokenizing.c
+++ src/token/tokenizing.c
@@ -6,12 +6,15 @@
 /*   By: asabri <asabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 15:31:47 by asabri            #+#    #+#             */
-/*   Updated: 2023/06/23 05:30:44 by asabri           ###   ########.fr       */
+/*   Updated: 2023/07/02 02:13:21 by asabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+
+
+// utils 
 t_token	*ft_lstlst(t_token *node)
 {
 	if (!node)
@@ -39,26 +42,33 @@ void	add_token_back(t_token **lst, t_token *new)
 	}
 }
 
-t_token *newtoken(char *tok)
+t_token *newtoken(char *tok,int hdoc,int expnd)
 {
     t_token *node;
-    int i;
 
-    i = -1;
     node = malloc(sizeof(t_token));
     node->token = tok;
     node->next = NULL;
+    node->bottom = NULL;
+    node->hd = false;
+    node->expand = false;
+    if (hdoc)
+        node->hd = true;
+    if (expnd)
+        node->expand = true;
     return (node);
 }
-// char *get_word(char *str, int index ,char c)
-// {
-//     int j;
+char *get_word(char *str, int index ,char c)
+{
+    int j;
 
-//     j = index;
-//     while (str[j] && str[j] != c)
-//         j++;
-//     return (ft_substr(str,index,j - 1));
-// }
+    j = index;
+    while (str[j] && !ft_strchr("\"\'<>()\t", str[j]))
+        j++;
+    return (ft_substr(str,index,j - 1));
+}
+
+
 void ft_initialize(t_init *init)
 {
     init->i = -1;
@@ -71,17 +81,41 @@ void ft_initialize(t_init *init)
     init->singl_quote = false;
     init->space = false;
 }
-
-
-void lexer_2()
+int onlyspace(int i, char *str)
 {
-    
+    while(str[i] && str[i] == ' ')
+        i++;
+    if (str[i])
+        return (0);
+    else 
+        return (1);
 }
+//-----------------------functions that give meanig to the line---------------------------
+
+void lexer_2(t_init *t, char *str)
+{
+    if(str[t->i] == '\'')
+        t->singl_quote = !t->singl_quote;
+    else if(str[t->i] == '\"')
+        t->double_quote = !t->double_quote;
+    else if(ft_strchr("<>",str[t->i]) && str[t->i] == str[t->i + 1])
+        t->unkown = 1;
+    else if (str[t->i] != '\"' || str[t->i] != '\'')
+        t->space = true;
+    else if (str[t->i] == '(')
+        t->op += 1;
+    else if (str[t->i] == ')')
+        t->cp += 1;
+    if (!onlyspace(t->i,str) && str[t->i] != '\'' && str[t->i] != '\"' 
+        && str[t->i] != ' ' && str[t->i] != '\t')
+        add_token_back(&t->token,newtoken())
+}
+
 void lexer_1(t_init *in, char *tok)
 {
     if (in->space)
-    {
-        
+    {   
+        add_token_back(newtoken(),)
         in->space = false;
     }
     else if(!in->space)
@@ -89,6 +123,9 @@ void lexer_1(t_init *in, char *tok)
         
     }
 }
+
+
+// main function that use everthing 
 t_token *strtoken(char *line)
 {
     t_token *token;
@@ -100,8 +137,10 @@ t_token *strtoken(char *line)
         if ((init->double_quote && line[init->i] =='\"') || (init->singl_quote && line[init->i] == '\''))
             lexer_1(&init,line);
         else if (ft_strchr("\"\'<>()\t", line[init->i]))
-        
+            lexer_2(&init,line);
         else if (!ft_strchr("\"\'<>() \t", line[init->i]) && (!init->double_quote|| !init->singl_quote))
+            
     }
+    
     return (NULL);
 }
